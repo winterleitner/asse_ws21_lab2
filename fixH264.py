ce69af0acbe31e125fb353d8fd3662bebb65f492
Matthias Hagmann <e01526023@student.tuwien.ac.at>
2021-12-23 18:01:37 +0100

    Detect I-frames

--- fixH264.py
+++ fixH264.py
@@ -15,6 +15,13 @@
 # [1] https://www.itu.int/rec/T-REC-H.264-202108-I/en
 # [2] https://github.com/gpac/gpac/wiki/MP4Box
 # [3] https://www.videolan.org/vlc/
+#
+# x264 - core 160 r3011 cde9a93 - H.264/MPEG-4 AVC codec - Copyleft 2003-2020 - http://www.videolan.org/x264.html - options:
+# cabac=1 ref=5 deblock=1:0:0 analyse=0x3:0x113 me=hex subme=8 psy=1 psy_rd=1.00:0.00 mixed_ref=1 me_range=16
+# chroma_me=1 trellis=2 8x8dct=1 cqm=0 deadzone=21,11 fast_pskip=1 chroma_qp_offset=-2 threads=12 lookahead_threads=2
+# sliced_threads=0 nr=0 decimate=1 interlaced=0 bluray_compat=0 constrained_intra=0 bframes=3 b_pyramid=2 b_adapt=1
+# b_bias=0 direct=3 weightb=1 open_gop=0 weightp=2 keyint=250 keyint_min=24 scenecut=40 intra_refresh=0 rc_lookahead=50
+# rc=crf mbtree=1 crf=23.0 qcomp=0.60 qpmin=0 qpmax=69 qpstep=4 ip_ratio=1.40 aq=1:1.00
 
 import hashlib
 import re
@@ -73,28 +80,32 @@ def findNALs(filename):
         correct = 0
         wrong = 0
         prev = 0
-        for m in re.finditer('(0{5}|0{7})1(..)', formatted_str):
+        for idx, m in enumerate(re.finditer('(0{5}|0{7})1(..)', formatted_str)):
             nal_unit = m.group(2)
             passed, correction = checkNAL(nal_unit)
             all = m.group(0)[1:]
 
             if passed:
-                print(
-                    f"{all}\t"
-                    f"NAL bin: {to_binary(nal_unit)[0:1]} {to_binary(nal_unit)[1:3]} {to_binary(nal_unit)[3:]}\t"
-                    f"zero: {get_forbidden_zero(nal_unit)}\t"
-                    f"ref_idc: {get_ref_idc(nal_unit)}\t"
-                    f"unit_type: {get_nal_type(nal_unit)}")
+                if get_ref_idc(nal_unit) == 3:
+                    print(
+                        f"{idx}\t"
+                        f"{all}\t"
+                        f"NAL bin: {to_binary(nal_unit)[0:1]} {to_binary(nal_unit)[1:3]} {to_binary(nal_unit)[3:]}\t"
+                        f"zero: {get_forbidden_zero(nal_unit)}\t"
+                        f"ref_idc: {get_ref_idc(nal_unit)}\t"
+                        f"unit_type: {get_nal_type(nal_unit)}")
                 correct += 1
             else:
-                print(
-                    f"{all}\t"
-                    f"NAL bin: {to_binary(nal_unit)[0:1]} {to_binary(nal_unit)[1:3]} {to_binary(nal_unit)[3:]}\t"
-                    f"zero: {get_forbidden_zero(nal_unit)}\t"
-                    f"ref_idc: {get_ref_idc(nal_unit)}\t"
-                    f"unit_type: {get_nal_type(nal_unit)}\t Wrong")
-                print(f"NAL: {m.group(0)[1:]} @ {m.start()} should be {m.group(0)[1:-2]}{int(correction, 2):02x}")
-                print()
+                if get_ref_idc(nal_unit) == 3:
+                    print(
+                        f"{idx}\t"
+                        f"{all}\t"
+                        f"NAL bin: {to_binary(nal_unit)[0:1]} {to_binary(nal_unit)[1:3]} {to_binary(nal_unit)[3:]}\t"
+                        f"zero: {get_forbidden_zero(nal_unit)}\t"
+                        f"ref_idc: {get_ref_idc(nal_unit)}\t"
+                        f"unit_type: {get_nal_type(nal_unit)}\t Wrong")
+                    print(f"NAL: {m.group(0)[1:]} @ {m.start()} should be {m.group(0)[1:-2]}{int(correction, 2):02x}")
+                    print()
                 wrong += 1
             prev = m.start()
 
@@ -107,12 +118,34 @@ def replace_error_nals(filename, target_filename):
         with open(filename, "rb", ) as h264_file:
             myline = h264_file.read()
             formatted_str = to_str(myline)
+            corrected = formatted_str
+
+            # ToDo find proper position of IDR frames
+            corrected = nth_repl_all(corrected, "000000167", "000000165", 2)  # set IDR frames every 2nd of 67
+
             corrected = re.sub(r'(0{5,7}1)6a', r"\g<1>0a", formatted_str)
-            #corrected = re.sub(r'(0{5,7}1)41', r"\g<1>45", corrected) makes it worse
+            # corrected = re.sub(r'(0{5,7}1)41', r"\g<1>45", corrected)  # unit type 2 to 5
+            # corrected = re.sub(r'(0{5,7}1)67', r"\g<1>65", corrected)  # unit type 7 to 5
             corrected = re.sub(r'(0{5,7}1)80', r"\g<1>00", corrected)  # fixes forbidden zero
             corrected = re.sub(r'(0{5,7}1)d4', r"\g<1>54", corrected)
             corrected = re.sub(r'(0{5,7}1)d8', r"\g<1>58", corrected)
+
             output.write(bytearray.fromhex(corrected))
+
+
+def nth_repl_all(s, sub, repl, nth):
+    find = s.find(sub)
+    # loop util we find no match
+    i = 1
+    while find != -1:
+        # if i is equal to nth we found nth matches so replace
+        if i == nth:
+            s = s[: find] + repl + s[find + len(sub): ]
+            i = 0
+        # find + len(sub) + 1 means we start after the last match
+        find = s.find(sub, find + len(sub) + 1)
+        i += 1
+    return s
 
 #[^0](0{5}|0{7})1(..)
 
@@ -139,8 +172,6 @@ def find_type_5(filename):
         myline = h264_file.read()
         formatted_str = to_str(myline)
     for m in re.finditer('(0{5}|0{7})1(..)', formatted_str):
-        # if m.end() % 2 != 0:
-        #    continue
         passed, correction = checkNAL(m.group(2))
         if passed and get_nal_type(m.group(2)) == "5":
             print(f"{m.group(0)[1:]}: {get_nal_type(m.group(2))} - {get_ref_idc(m.group(2))}")
